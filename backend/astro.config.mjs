a3168cacea3d608698347e9f6bb64fdbca27b145
Vladimir Lukyanov <vladimir@lukyanov.net>
2024-03-05 10:13:44 +0000

    v0.0.1479
    - scss update

--- backend/astro.config.mjs
+++ backend/astro.config.mjs
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import add_csh_nonce from "./src/lib/utils/add_csh_nonce.ts";
@@ -11,7 +12,16 @@ export default defineConfig({
   outDir: "../frontend/",
   site: "https://nulllab.net",
   integrations: [mdx(), sitemap(), add_csh_nonce(), sitemap({})],
+  compressHTML: true,
+  scopedStyleStrategy: "where",
   server: {
     host: true,
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "~": path.resolve(path.dirname(""), "./"),
+      },
+    },
   },
 });
