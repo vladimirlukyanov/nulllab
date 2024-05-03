fe1d0efcfc261a2942da22fba4881ae10e0c8b5e
vladimirlukyanov <vladimir@lukyanov.net>
2024-05-03 22:37:27 +0300

    v0.0.161 – package json update – content update

--- backend/src/content/config.ts
+++ backend/src/content/config.ts
@@ -8,8 +8,8 @@ const blog = defineCollection({
       title: z.string(),
       description: z.string().optional(),
       // Transform string to Date object
-      published_date: z.coerce.date(),
-      updatedDate: z.coerce.date().optional(),
+      published_date: z.date(),
+      updatedDate: z.date().optional(),
       heroImage: z.string().optional(),
       category: z.string(),
       type: z.string().optional(),
