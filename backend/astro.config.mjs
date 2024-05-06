113eacefb14c06c0dd2e814cb8aa3a99e52fca90
vladimirlukyanov <vladimir@lukyanov.net>
2024-05-06 04:37:04 +0300

    v0.0.167 - image service update

--- backend/astro.config.mjs
+++ backend/astro.config.mjs
@@ -1,34 +1,41 @@
 import path from "path";
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import add_csh_nonce from "./src/lib/utils/add_csh_nonce.ts";
-import sitemap from "@astrojs/sitemap";
+import {defineConfig} from "astro/config";
+import {imageService} from "@unpic/astro/service";
 
+
+import add_csh_nonce from "./src/lib/utils/add_csh_nonce.ts";
+
+import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  compressHTML: true,
-  outDir: "../frontend/",
-  site: "https://nulllab.net",
-  markdown: {
-    syntaxHighlight: false,
-  },
-  integrations: [mdx({
-    optimize: {
-
+    compressHTML: true,
+    outDir: "../frontend/",
+    site: "https://nulllab.net",
+    markdown: {
+        syntaxHighlight: false,
     },
-  }), sitemap(), add_csh_nonce(), sitemap({})],
-  compressHTML: true,
-  scopedStyleStrategy: "where",
-  server: {
-    host: true,
-  },
-  vite: {
-    resolve: {
-      alias: {
-        "~": path.resolve(path.dirname(""), "./"),
-      },
+    integrations: [
+        mdx({
+            optimize: {},
+        }),
+        sitemap(),
+        add_csh_nonce(),
+        sitemap({})
+    ],
+    scopedStyleStrategy: "where",
+    image: {
+        service: imageService(),
     },
-  },
+    server: {
+        host: true,
+    },
+    vite: {
+        resolve: {
+            alias: {
+                "~": path.resolve(path.dirname(""), "./"),
+            },
+        },
+    },
 });
