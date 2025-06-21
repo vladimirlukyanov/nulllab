6e5b606029371ff49f5146dca76f0be83335ec5a
Vladimir Lukyanov <vladimir@lukyanov.net>
2025-06-21 18:20:08 +0300

    v0.0.54 – updated content – updated config – added categories – added tags – added schema – added author – related posts added

--- backend/src/content.config.mjs
+++ backend/src/content.config.mjs
@@ -3,55 +3,57 @@ import {defineCollection, reference, z} from "astro:content";
 import {glob} from 'astro/loaders';
 
 const blog = defineCollection({
-  loader: glob({ base: './src/content/blog', pattern: '**/*.*' }),
+  loader: glob({base: './src/content/blog', pattern: '**/*.*'}),
   schema: ({image}) => z.object({
-        title: z.string(),
-        description: z.string().optional(),
+    title: z.string(),
+    description: z.string().optional(),
 
-        category: z.string(),
+    authors: z.array(reference('authors')),
+    categories: z.array(reference('categories')),
+    tags: z.array(reference('tags')).optional(),
 
-        type: z.string().optional(),
-        tags: z.array(z.string()).optional(),
+    type: z.string().optional(),
 
-        featured: z.boolean().optional(),
+    featured: z.boolean().optional(),
 
-        cover: image().optional(),
+    cover: image().optional(),
 
-        image: z
-            .object({
-                url: image(),
-                alt: z.string(),
-            })
-            .optional(),
+    image: z
+        .object({
+          url: image(),
+          alt: z.string(),
+        })
+        .optional(),
 
-        related_posts : z.array(reference('blog')).optional(),
+    related_posts: z.array(reference('blog')).optional(),
 
-        published_date: z.date(),
-        updated_date: z.date().optional(),
-    }),
+    published_date: z.date(),
+    updated_date: z.date().optional(),
+  }),
 });
 
 const categories = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.json', base: "./src/content/categories" }),
+  loader: glob({base: './src/content/categories', pattern: '**/*.*'}),
   schema: z.object({
     name: z.string(),
-    portfolio: z.string().url(),
+    description: z.string().optional()
   })
 });
 
 const tags = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.json', base: "./src/content/tags" }),
+  loader: glob({base: './src/content/tags', pattern: '**/*.*'}),
   schema: z.object({
     name: z.string(),
-    portfolio: z.string().url(),
+    description: z.string().optional()
   })
 });
 
 const authors = defineCollection({
-  loader: glob({ pattern: '**/[^_]*.json', base: "./src/content/authors" }),
+  loader: glob({base: './src/content/authors', pattern: '**/*.*'}),
   schema: z.object({
     name: z.string(),
-    portfolio: z.string().url(),
+    description: z.string().optional(),
+    position: z.string().optional(),
   })
 });
 
