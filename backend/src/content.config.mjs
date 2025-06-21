ecc1f699d8f19a2af494d7bdcd5fe2f8ae55a4ec
Vladimir Lukyanov <vladimir@lukyanov.net>
2025-06-21 16:11:31 +0300

    v0.0.53 – updated collections – update blog content

--- backend/src/content.config.mjs
+++ backend/src/content.config.mjs
@@ -1,32 +1,58 @@
-import {defineCollection, z} from "astro:content";
+import {defineCollection, reference, z} from "astro:content";
 
 import {glob} from 'astro/loaders';
 
 const blog = defineCollection({
   loader: glob({ base: './src/content/blog', pattern: '**/*.*' }),
   schema: ({image}) => z.object({
-        // Transform string to Date object
-        published_date: z.date(),
-        updatedDate: z.date().optional(),
-        heroImage: z.string().optional(),
-
         title: z.string(),
         description: z.string().optional(),
 
         category: z.string(),
+
         type: z.string().optional(),
         tags: z.array(z.string()).optional(),
 
         featured: z.boolean().optional(),
 
         cover: image().optional(),
+
         image: z
             .object({
                 url: image(),
                 alt: z.string(),
             })
             .optional(),
+
+        related_posts : z.array(reference('blog')).optional(),
+
+        published_date: z.date(),
+        updated_date: z.date().optional(),
     }),
 });
 
-export const collections = {blog};
+const categories = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.json', base: "./src/content/categories" }),
+  schema: z.object({
+    name: z.string(),
+    portfolio: z.string().url(),
+  })
+});
+
+const tags = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.json', base: "./src/content/tags" }),
+  schema: z.object({
+    name: z.string(),
+    portfolio: z.string().url(),
+  })
+});
+
+const authors = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.json', base: "./src/content/authors" }),
+  schema: z.object({
+    name: z.string(),
+    portfolio: z.string().url(),
+  })
+});
+
+export const collections = {blog, categories, tags, authors};
