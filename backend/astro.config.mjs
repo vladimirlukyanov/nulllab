45de3539a80bb1afec7cf820d255a1693e013048
vladimirlukyanov <vladimir@lukyanov.net>
2025-07-06 19:28:31 +0300

    v0.0.624 – updated content – added external links

--- backend/astro.config.mjs
+++ backend/astro.config.mjs
@@ -4,46 +4,43 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-import {defineConfig} from "astro/config";
-import {imageService} from "@unpic/astro/service";
+import { defineConfig } from 'astro/config'
+import { imageService } from '@unpic/astro/service'
 
+import add_csh_nonce from './src/lib/utils/add_csh_nonce.ts'
 
-import add_csh_nonce from "./src/lib/utils/add_csh_nonce.ts";
-
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  outDir: "../frontend/",
-  site: "https://nulllwab.net",
+  outDir: '../frontend/',
+  site: 'https://nulllwab.net',
   markdown: {
     syntaxHighlight: false,
   },
   integrations: [
-    mdx({
-      optimize: {},
-    }),
+    mdx(),
     sitemap(),
     add_csh_nonce(),
-    sitemap({})
+    sitemap(),
   ],
-  scopedStyleStrategy: "where",
+  scopedStyleStrategy: 'where',
   image: {
     service: imageService(),
   },
   server: {
     host: true,
   },
-  security : {
-    checkOrigin: true
+  security: {
+    checkOrigin: true,
   },
   vite: {
     resolve: {
       alias: {
-        '~': path.resolve(__dirname, './src')
-      }
+        '~': path.resolve(__dirname, './src'),
+      },
     },
   },
-});
+})
