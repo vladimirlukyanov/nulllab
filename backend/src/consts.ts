823396db6b0c53b9cb38c68a978ce6fc50e4aaca
Vladimir Lukyanov <vladimir@lukyanov.net>
2026-03-09 01:11:09 +0300

    v0.1.545 – <head> title update

--- backend/src/consts.ts
+++ backend/src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'NullLab';
-export const SITE_DESCRIPTION = 'Development & Research';
+export const SITE_DESCRIPTION = 'Exploring the intersection of technology, data, and innovative engineering. Nulllab shares insights on software development, system architecture, and solving complex problems with code.';
