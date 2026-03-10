b0139c66730b90de7be9360278fce75da570f1af
Vladimir Lukyanov <vladimir@lukyanov.net>
2026-03-10 17:30:36 +0300

    v0.1.549 – keywords meta added

--- backend/src/consts.ts
+++ backend/src/consts.ts
@@ -3,3 +3,4 @@
 
 export const SITE_TITLE = 'NullLab';
 export const SITE_DESCRIPTION = 'Exploring the intersection of technology, data, and innovative engineering. Nulllab shares insights on software development, system architecture, and solving complex problems with code.';
+export const SITE_KEYWORDS = 'software development, tech research, technology experiments, development blog, anime reviews, anime blog, tech culture, coding projects, NullLab, tech portfolio, anime commentary, development and research, tech experiments';
